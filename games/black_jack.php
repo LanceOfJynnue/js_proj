159337ace4df102d488b0bc87d7845dbca8909eb
Lance Morales <earllance.morales@gmail.com>
2026-07-20 12:04:16 +0800

    first commit

---
+++ games/black_jack.php
@@ -0,0 +1,22 @@
+<?php require('../mysql_db.php'); ?>
+
+<html>
+   <link rel="stylesheet" href="../style.css">
+<head>
+   <title>Black Jack</title>
+   <script type="text/javascript">
+
+   function getReport(){
+
+   } 
+
+   </script>
+   <a class="abutton" style="text-decoration: none;" href="../index.php">Go Back to Hub</a>
+</head>
+<body>     
+   <fieldset style="margin:auto;width:90%;margin-bottom:10px;">
+      <legend><h1>Black Jack</h1></legend>
+      <h2>Welcome to "Black Jack" game!</h2>
+   </fieldset>
+</body>
+</html>
---
+++ games/rps.php
@@ -0,0 +1,22 @@
+<?php require('../mysql_db.php'); ?>
+
+<html>
+    <link rel="stylesheet" href="../style.css">
+<head>
+    <title>Rock, Paper, Scissors</title>
+    <script type="text/javascript">
+
+    function getReport(){
+
+    } 
+
+    </script>
+    <a class="abutton" style="text-decoration: none;" href="../index.php">Go Back to Hub</a>
+</head>
+<body>     
+    <fieldset style="margin:auto;width:90%;margin-bottom:10px;">
+        <legend><h1>Rock, Paper, Scissors</h1></legend>
+        <h2>Welcome to "Rock, Paper, Scissors" game!</h2>
+    </fieldset>
+</body>
+</html>
