bbfc7b6998a115f91cde2d1f7d869301ab485d04
Lance Morales <earllance.morales@gmail.com>
2026-08-18 21:26:17 +0800

    modified:   js_games.js

--- js_games.js
+++ js_games.js
@@ -1,9 +1,23 @@
+// Call the function when the page loads
+document.addEventListener('DOMContentLoaded', getTodos);
+
+const heading = document.getElementById('main-heading');
+const button = document.getElementById('action-btn');
+
 // Replace these with your actual Supabase project values
 const SUPABASE_URL = 'https://wphnnfrjvmczpggrknay.supabase.co/rest/v1/';
 const SUPABASE_ANON_KEY = 'sb_publishable_zwSiHqLvzwa3ssztWbsJ9Q_gK-IYCkZ';
 
 // The CDN automatically exposes 'supabase' globally
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+button.addEventListener('click', () => {
+    heading.textContent = "JavaScript is Working!";
+    
+    heading.style.color = "#28a745";
+    
+    alert("You successfully manipulated the HTML with JavaScript!");
+});
 
 // Example: Fetch data from a table named 'todos'
 async function getData() {
@@ -18,17 +32,3 @@ async function getData() {
   console.log('Your data:', data);
   alert('Your data:', data);
 }
-
-// Call the function when the page loads
-document.addEventListener('DOMContentLoaded', getTodos);
-
-const heading = document.getElementById('main-heading');
-const button = document.getElementById('action-btn');
-
-button.addEventListener('click', () => {
-    heading.textContent = "JavaScript is Working!";
-    
-    heading.style.color = "#28a745";
-    
-    alert("You successfully manipulated the HTML with JavaScript!");
-});
